d99fd3a4413b0de2029a7a2b0e34c7563094613e
RikkaW <rikka@shizuku.moe>
2020-01-08 00:24:07 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Riru - Template
+
+[Riru](https://github.com/RikkaApps/Riru) module template.
+
+## Build
+
+1. Replace all "template" to your name
+   
+   * `module/build.gradle`: "template" in variable `moduleId`, `moduleProp`
+   * `module/src/main/cpp/CMakeLists.txt`: "riru_template" in `set(MODULE_NAME )`
+   
+2. Write you codes
+3. Run gradle task `:module:assembleRelease` task from Android Studio or command line, zip will be saved in `out`.
+
+## Notes
+
+* DO NOT overwrite `android.os.SystemProperties#native_set` in core, or your data may be wiped
+  ([Detail info](https://github.com/RikkaApps/Riru/blob/v7/riru-core/jni/main/jni_native_method.cpp#L162-L176))
+  (If you really need to hook this, remember to clear exception)
+* DO NO print log (`__android_log_print`) in `nativeForkAndSpecialize(Pre/Post)` `nativeForkSystemServer(Pre/Post)` when in zygote process, or it may cause zygote not work
+  (magic not confirmed, [Detail info](https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66))
+* Add `-ffixed-x18` to both compiler and linker parameter, or it will cause problems on Android Q (see template)
---
+++ module/src/main/cpp/main.cpp
@@ -0,0 +1,68 @@
+#include <jni.h>
+#include <sys/types.h>
+
+// You can remove functions you don't need
+
+extern "C" {
+#define EXPORT __attribute__((visibility("default"))) __attribute__((used))
+EXPORT void nativeForkAndSpecializePre(
+        JNIEnv *env, jclass clazz, jint *_uid, jint *gid, jintArray *gids, jint *runtimeFlags,
+        jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
+        jintArray *fdsToClose, jintArray *fdsToIgnore, jboolean *is_child_zygote,
+        jstring *instructionSet, jstring *appDataDir, jstring *packageName,
+        jobjectArray *packagesForUID, jstring *sandboxId) {
+    // packageName, packagesForUID, sandboxId are added from Android Q beta 2, removed from beta 5
+}
+
+EXPORT int nativeForkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
+    if (res == 0) {
+        // in app process
+    } else {
+        // in zygote process, res is child pid
+        // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
+    }
+    return 0;
+}
+
+EXPORT __attribute__((visibility("default"))) void specializeAppProcessPre(
+        JNIEnv *env, jclass clazz, jint *_uid, jint *gid, jintArray *gids, jint *runtimeFlags,
+        jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
+        jboolean *startChildZygote, jstring *instructionSet, jstring *appDataDir,
+        jstring *packageName, jobjectArray *packagesForUID, jstring *sandboxId) {
+    // this is added from Android Q beta, but seems Google disabled this in following updates
+
+    // packageName, packagesForUID, sandboxId are added from Android Q beta 2, removed from beta 5
+}
+
+EXPORT __attribute__((visibility("default"))) int specializeAppProcessPost(
+        JNIEnv *env, jclass clazz) {
+    // this is added from Android Q beta, but seems Google disabled this in following updates
+    return 0;
+}
+
+EXPORT void nativeForkSystemServerPre(
+        JNIEnv *env, jclass clazz, uid_t *uid, gid_t *gid, jintArray *gids, jint *runtimeFlags,
+        jobjectArray *rlimits, jlong *permittedCapabilities, jlong *effectiveCapabilities) {
+
+}
+
+EXPORT int nativeForkSystemServerPost(JNIEnv *env, jclass clazz, jint res) {
+    if (res == 0) {
+        // in system server process
+    } else {
+        // in zygote process, res is child pid
+        // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
+    }
+    return 0;
+}
+
+EXPORT int shouldSkipUid(int uid) {
+    // by default, Riru only call module functions in "normal app processes" (10000 <= uid % 100000 <= 19999)
+    // false = don't skip
+    return false;
+}
+
+EXPORT void onModuleLoaded() {
+    // called when the shared library of Riru core is loaded
+}
+}

322576c630974fadbf73cc7e95fe7e0ce405534b
RikkaW <rikka@shizuku.moe>
2020-02-24 18:44:50 +0800

    fix bug

--- template/magisk_module/riru.sh
+++ template/magisk_module/riru.sh
@@ -13,15 +13,18 @@ check_riru_version() {
   if [ ! -f "$RIRU_PATH/api_version" ]; then
     ui_print    "*********************************************************"
     ui_print    "! Riru is not installed"
-    ui_print    "! Please download 'Riru - Core' from 'Magisk Manager' or https://github.com/RikkaApps/Riru/releases"
+    ui_print    "! Please install Riru (Riru - Core) from Magisk Manager or https://github.com/RikkaApps/Riru/releases"
+    ui_print    "! If you have just installed Riru, please reboot"
     abort_clean "*********************************************************"
   fi
-  RIRU_API_VERSION=$(cat "$RIRU_PATH/api_version")
+  RIRU_API_VERSION=$(cat "$RIRU_PATH/api_version") || RIRU_API_VERSION=0
+  [ "$RIRU_API_VERSION" -eq "$RIRU_API_VERSION" ] || RIRU_API_VERSION=0
   ui_print "- Riru API version: $RIRU_API_VERSION"
   if [ "$RIRU_API_VERSION" -lt $RIRU_MIN_API_VERSION ]; then
     ui_print    "*********************************************************"
     ui_print    "! The latest version of Riru is required"
-    ui_print    "! Please download 'Riru - Core' from 'Magisk Manager' or https://github.com/RikkaApps/Riru/releases"
+    ui_print    "! Please install Riru (Riru - Core) from Magisk Manager or https://github.com/RikkaApps/Riru/releases"
+    ui_print    "! If you have just installed Riru, please reboot"
     abort_clean "*********************************************************"
   fi
 }

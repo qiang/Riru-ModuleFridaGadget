890f50f643b607ede1303dc54057fe2d0639519a
RikkaW <rikka@xing.moe>
2020-10-09 23:21:53 +0800

    update for api 8

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,11 +1,8 @@
 #include <jni.h>
 #include <sys/types.h>
+#include <riru.h>
 
-// You can remove functions you don't need
-
-extern "C" {
-#define EXPORT __attribute__((visibility("default"))) __attribute__((used))
-EXPORT void nativeForkAndSpecializePre(
+static void forkAndSpecializePre(
         JNIEnv *env, jclass clazz, jint *_uid, jint *gid, jintArray *gids, jint *runtimeFlags,
         jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
         jintArray *fdsToClose, jintArray *fdsToIgnore, jboolean *is_child_zygote,
@@ -13,17 +10,16 @@ EXPORT void nativeForkAndSpecializePre(
         jobjectArray *whitelistedDataInfoList, jboolean *bindMountAppDataDirs, jboolean *bindMountAppStorageDirs) {
 }
 
-EXPORT int nativeForkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
+static void forkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
     if (res == 0) {
         // in app process
     } else {
         // in zygote process, res is child pid
         // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
     }
-    return 0;
 }
 
-EXPORT __attribute__((visibility("default"))) void specializeAppProcessPre(
+static void specializeAppProcessPre(
         JNIEnv *env, jclass clazz, jint *_uid, jint *gid, jintArray *gids, jint *runtimeFlags,
         jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
         jboolean *startChildZygote, jstring *instructionSet, jstring *appDataDir,
@@ -32,35 +28,51 @@ EXPORT __attribute__((visibility("default"))) void specializeAppProcessPre(
     // added from Android 10, but disabled at least in Google Pixel devices
 }
 
-EXPORT __attribute__((visibility("default"))) int specializeAppProcessPost(
+static void specializeAppProcessPost(
         JNIEnv *env, jclass clazz) {
     // added from Android 10, but disabled at least in Google Pixel devices
-    return 0;
 }
 
-EXPORT void nativeForkSystemServerPre(
+static void forkSystemServerPre(
         JNIEnv *env, jclass clazz, uid_t *uid, gid_t *gid, jintArray *gids, jint *runtimeFlags,
         jobjectArray *rlimits, jlong *permittedCapabilities, jlong *effectiveCapabilities) {
 
 }
 
-EXPORT int nativeForkSystemServerPost(JNIEnv *env, jclass clazz, jint res) {
+static void forkSystemServerPost(JNIEnv *env, jclass clazz, jint res) {
     if (res == 0) {
         // in system server process
     } else {
         // in zygote process, res is child pid
         // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
     }
-    return 0;
 }
 
-EXPORT int shouldSkipUid(int uid) {
+int shouldSkipUid(int uid) {
     // by default, Riru only call module functions in "normal app processes" (10000 <= uid % 100000 <= 19999)
     // false = don't skip
     return false;
 }
 
-EXPORT void onModuleLoaded() {
+static void onModuleLoaded() {
     // called when the shared library of Riru core is loaded
+}
+
+extern "C" {
+void init(Riru *_riru) {
+    riru = _riru;
+    
+    riru->module->supportHide = false;
+
+    // You can remove functions you don't need
+    riru->module->apiVersion = RIRU_MODULE_API_VERSION;
+    riru->module->onModuleLoaded = onModuleLoaded;
+    riru->module->shouldSkipUid = shouldSkipUid;
+    riru->module->forkAndSpecializePre = forkAndSpecializePre;
+    riru->module->forkAndSpecializePost = forkAndSpecializePost;
+    riru->module->specializeAppProcessPre = specializeAppProcessPre;
+    riru->module->specializeAppProcessPost = specializeAppProcessPost;
+    riru->module->forkSystemServerPre = forkSystemServerPre;
+    riru->module->forkSystemServerPost = forkSystemServerPost;
 }
 }
